abcc367329be5f92654548c7a77a7119f46c5521
Boris Castillo <bocast96@terpmail.umd.edu>
2018-03-06 00:04:58 -0500

    changing state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: {name:"Boris", age: 31}
+  }
+
+  changeNameHandler = () => {
+    console.log("button pushed");
+    this.setState( {
+      person: {name: 'Pulento', age:69}
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1> HELLO WORLD!! </h1>
-        <Person name='Boris' age='22'> I like boobs </Person>
+        <button onClick={this.changeNameHandler}> Change Name </button>
+        <Person name={this.state.person.name} age={this.state.person.age}> I like boobs </Person>
       </div>
     );
   }
